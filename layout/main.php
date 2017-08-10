0c9f68d3441103b553c8088895d841b4b6dc0431
ptasiek3003 <ptasiek3003@gmail.com>
2017-08-10 14:24:55 +0200

    layout

---
+++ layout/main.php
@@ -0,0 +1,18 @@
+<div class="col-md-8">
+  <div class="panel panel-info">
+    <div class="panel-heading">
+    <h3 class="panel-title">Products</h3>
+    </div>
+    <div class="panel-body">
+
+    <div id="get_prod">
+
+    <!-- get product  wit jquery ajacx -->
+    </div>
+
+
+    </div>
+    <div class="panel-footer">&copy; 2016</div>
+  </div>
+</div>
+</div>
